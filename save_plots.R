ff4077eb7a693770a865a0ad66313621b388e990
DanielQuiroz97 <cristianquirozd1997@gmail.com>
2019-07-15 12:21:58 -0500

    second final draft

---
+++ save_plots.R
@@ -0,0 +1,21 @@
+ggsave(filename = "sample_space.jpg", plot = sspace_plot, path = "./plots",
+       width = 6, height = 4, units = "in", dpi = 350, scale = 1.35)
+
+ggsave(filename = "medios_comunicacion.jpg", plot = medios_plot, path = "./plots",
+       width = 6, height = 4, units = "in", dpi = 350, scale = 1.35)
+
+ggsave(filename = "main_plot.jpg", plot = main_plot, path = "./plots",
+       width = 6, height = 4, units = "in", dpi = 350, scale = 1.4)
+
+ggsave(filename = "ventajas_desventjas.jpg", plot = advantages, path = "./plots",
+       width = 6, height = 4, units = "in", dpi = 350, scale = 1.2)
+
+ggsave(filename = "OGM_products.jpg", plot = products_plot, path = "./plots",
+       width = 6, height = 4, units = "in", dpi = 350, scale = 1.2)
+
+
+ggsave(filename = "tech_perception.jpg", plot = tech_perception, path = "./plots",
+       width = 6, height = 4, units = "in", dpi = 350, scale = 1.2)
+
+ggsave(filename = "consumption.jpg", plot = consumption_plot, path = "./plots",
+       width = 6, height = 4, units = "in", dpi = 350, scale = 1.7)
